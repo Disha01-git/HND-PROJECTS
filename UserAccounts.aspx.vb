8e5c01906ba17d7f12143700e4b3bde8955a2207
Disha01-git <55386263+Disha01-git@users.noreply.github.com>
2019-10-06 15:13:11 +0100

    Delete UserAccounts.aspx.vb

--- UserAccounts.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿
-Partial Class Administration_UserAccounts
-    Inherits System.Web.UI.Page
-
-    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        DyForm.InnerHtml = "Name : <input type = text  value = ""+  +""/>"
-    End Sub
-End Class
